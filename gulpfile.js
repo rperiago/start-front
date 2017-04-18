1c9a66134074e7a0079737969a36bc7b8af730fb
rperiago <periago.ricardo@gmail.com>
2017-04-18 19:21:05 -0300

    Agora com pug

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,5 @@
+'use strict'
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
@@ -5,51 +7,65 @@ var rename = require('gulp-rename');
 var browserSync = require('browser-sync');
 var minifycss = require('gulp-minify-css');
 var autoprefixer = require('gulp-autoprefixer');
+var pug = require('gulp-pug2')
 var mainBowerFiles = require('main-bower-files');
 var bourbon = require("node-bourbon").includePaths;
 var neat = require("node-neat").includePaths;
 var reload = browserSync.reload;
 
-gulp.task('sass', function() {
-	gulp.src('app/scss/style.scss')
-			.pipe(sass({
-				includePaths: ['app/scss'],
-				includePaths: bourbon,
-				includePaths: neat,
-			}))
-			.pipe(gulp.dest('app/styles'))
-			.pipe(reload({ stream:true }))
-			.pipe(rename({suffix: '.min'}))
-			.pipe(minifycss())
-			.pipe(gulp.dest('app/styles'))
+gulp.task('sass', function () {
+  gulp.src('app/scss/style.scss')
+    .pipe(sass({
+      includePaths: ['app/scss'],
+      includePaths: bourbon,
+      includePaths: neat,
+    })).on('error', sass.logError)
+    .pipe(gulp.dest('app/styles'))
+    .pipe(reload({stream: true}))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('app/styles'))
 });
 
-gulp.task("bower-files", function(){
-	return gulp.src(mainBowerFiles(/* options */), { base: 'bower_components' })
-        .pipe(gulp.dest("app/scripts/libs/"))
+gulp.task("bower-files", function () {
+  return gulp.src(mainBowerFiles(/* options */), {base: 'bower_components'})
+    .pipe(gulp.dest("app/scripts/libs/"))
 });
 
-gulp.task('uglify', function() {
-	gulp.src('app/scripts/main.js')
-			.pipe(uglify())
-			.pipe(rename({suffix: '.min'}))
-			.pipe(gulp.dest('app/scripts'))
-			.pipe(reload({ stream:true }))
-})
+gulp.task('uglify', function () {
+  gulp.src('app/scripts/main.js')
+    .pipe(uglify())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('app/scripts'))
+    .pipe(reload({stream: true}))
+});
+gulp.task('views:render', function() {
+    return gulp.src('app/views/**/*.pug')
+        .pipe(pug({ yourTemplate: 'Locals' }))
+        .pipe(gulp.dest('app/'))
+        // => build/views/example.html
+});
+
+gulp.task('views:compile', function() {
+    return gulp.src('app/views/**/*.pug')
+        .pipe(pug.compile())
+        .pipe(gulp.dest('app/'))
+});
 
 // observa mudanças em arquivos na pasta SCSS e SCRIPTS
-gulp.task('serve', ['sass','bower-files'], function() {
-	browserSync({
-		server:{
-			baseDir: 'app'
-		}
-	})
+gulp.task('serve', ['sass', 'bower-files', 'views:render'], function () {
+  browserSync({
+    server: {
+      baseDir: 'app'
+    }
+  });
 
-	gulp.watch('app/scripts/**/*.js', ['uglify'])
-	gulp.watch('app/scss/**/*.scss', ['sass'])
-	gulp.watch(
-		['*.html', 'scripts/**/*.js'],
-		{cwd:'app'},
-		reload
-	)
-})
+  gulp.watch('app/scripts/**/*.js', ['uglify'])
+  gulp.watch('app/scss/**/*.scss', ['sass'])
+  gulp.watch('app/views/**/*.pug', ['views:render'])
+  gulp.watch(
+    ['*.html', 'scripts/**/*.js'],
+    {cwd: 'app'},
+    reload
+  )
+});
